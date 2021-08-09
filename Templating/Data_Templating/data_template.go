e281992e622f39178ec00da79ed8b58006440b33
odzen <jsebastian.va@gmail.com>
2021-08-10 00:09:07 +0300

    Comment .gohtml

--- Templating/Data_Templating/data_template.go
+++ Templating/Data_Templating/data_template.go
@@ -15,7 +15,7 @@ func init(){
 func main() {
 
 	//So in this case, we pass the data, it is not nil, it is 42
-	// It will print the file, with the data 42
+	// It will print the file, with the data 42 which is an int
 	err:= tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml",42)
 	if err!= nil{
 		log.Fatalln(err)
