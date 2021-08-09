cc744ae28e12e1fe8cd0fb597a8939edff0fe222
odzen <jsebastian.va@gmail.com>
2021-08-10 00:09:47 +0300

    Comment .gohtml

--- Templating/Value-template/value_template.go
+++ Templating/Value-template/value_template.go
@@ -16,6 +16,8 @@ func main() {
 	//Step 1: Passed in the data
 	//Step 2: Took the value of the data and assing it to a variable, $wisdom in the file .gohtml
 	//Step 3: I'm printing that variable out.
+
+	// This time we passed the value of type String
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", `Release self-focus; embrace other-focus.`)
 	if err != nil {
 		log.Fatalln(err)
