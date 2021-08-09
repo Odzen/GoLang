588268f8f36b0b0b8634efefa376d7780375fd6f
odzen <jsebastian.va@gmail.com>
2021-08-09 22:40:02 +0300

    Passing data into a template

---
+++ Templating/Data_Templating/data_template.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init(){
+	 tpl= template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+
+	//So in this case, we pass the data, it is not nil, it is 42
+	// It will print the file, with the data 42
+	err:= tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml",42)
+	if err!= nil{
+		log.Fatalln(err)
+	}
+}
