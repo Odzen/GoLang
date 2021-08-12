3793aa64275fd95abccdebe140cbbf81f809adb8
odzen <jsebastian.va@gmail.com>
2021-08-12 04:38:30 +0300

    Index only with slide of strings

--- Templating/FunctionsTemplating/PredefinedGlobalFunctions/Index/index.go
+++ Templating/FunctionsTemplating/PredefinedGlobalFunctions/Index/index.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import(
+	"log"
+	"os"
+	"text/template"
+)
 
+var tpl *template.Template
+
+func init() {
+	tpl= template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	//Data
+	xs:= []string{"zero", "one", "two","three", "four","five"}
+
+	//Execute Template
+	err:= tpl.Execute(os.Stdout, xs)
+
+	if err != nil{
+		log.Fatalln(err)
+	}
 }
