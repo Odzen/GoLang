3b529da061144c3df9cc0741b1228c3025e389c4
odzen <jsebastian.va@gmail.com>
2021-08-16 02:04:05 +0300

    Passing data toa a nested template

---
+++ Templating/Nested_Templates/DataToTemplate/nested_data.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl= template.Must(template.ParseGlob("Templates/*.gohtml"))
+}
+
+func main() {
+
+	//Execute the main template, in this case, the index.gohtml
+	err:= tpl.ExecuteTemplate(os.Stdout, "index.gohtml",42)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
